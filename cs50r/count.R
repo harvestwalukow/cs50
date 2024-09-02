c26cefad303690c9e32a0930bca22d2ecc5597ea
harvest <harvest0805@gmail.com>
2024-09-02 09:51:12 +0700

    update

--- cs50r/count.R
+++ cs50r/count.R
@@ -1,7 +1,11 @@
-mario <- as.integer(readline("Enter votes for Mario: "))
-peach <- as.integer(readline("Enter votes for Peach: "))
-bowser <- as.integer(readline("Enter votes for Bowser: "))
+get_votes <- function(prompt = "Enter votes: ") {
+  votes <- suppressWarnings(as.integer(readline(prompt)))
+  ifelse(is.na(votes), 0, votes)
+}
+
+mario <- get_votes()
+peach <- get_votes()
+bowser <- get_votes()
 
 total <- sum(mario, peach, bowser)
-
-print(paste("Total votes:", total))
+cat("Total votes:", total)
